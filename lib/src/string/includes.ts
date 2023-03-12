e2244751a07b4d298cf830a0cb3685028749af71
nev21 <82737406+nev21@users.noreply.github.com>
2023-03-11 17:42:37 -0800

    Add strIncludes, strContains

---
+++ lib/src/string/includes.ts
@@ -0,0 +1,75 @@
+/*
+ * @nevware21/ts-utils
+ * https://github.com/nevware21/ts-utils
+ *
+ * Copyright (c) 2022 Nevware21
+ * Licensed under the MIT license.
+ */
+
+import { isRegExp } from "../helpers/base";
+import { dumpObj } from "../helpers/diagnostics";
+import { throwTypeError } from "../helpers/throw";
+import { StrProto } from "../internal/constants";
+import { _unwrapFunction } from "../internal/unwrapFunction"
+import { asString } from "./as_string";
+import { strIndexOf } from "./index_of";
+
+/**
+ * The strIncludes() method performs a __case-sensitive__ search to determine whether one string
+ * may be found within another string, returning `true` or `false` as appropriate.
+ *
+ * This method lets you determine whether or not a string includes another string.
+ * @since 0.9.0
+ * @group String
+ * @param value - The string value to be searched.
+ * @param searchString - A string to be searched for within the value. Cannot be a regex. All
+ * values that are not regexes are coerced to strings, so omitting it or passing undefined
+ * causes `strIncludes()` to search for the string "undefined", which is rarely what you want.
+ * @param position - The position within the string at which to begin searching for searchString. (Defaults to 0.)
+ * @returns `true` if the search string is found anywhere within the given string value, including
+ * when searchString is an empty string; otherwise, `false`.
+ * @throws TypeError If searchString is a regex.
+ */
+export const strIncludes: (value: string, searchString: string, position?: number) => boolean = _unwrapFunction("includes", StrProto, polyStrIncludes);
+
+/**
+ * The strContains() method performs a __case-sensitive__ search to determine whether one string
+ * may be found within another string, returning `true` or `false` as appropriate.
+ *
+ * This method lets you determine whether or not a string includes another string.
+ * @since 0.9.0
+ * @group String
+ * @param value - The string value to be searched.
+ * @param searchString - A string to be searched for within the value. Cannot be a regex. All
+ * values that are not regexes are coerced to strings, so omitting it or passing undefined
+ * causes `strContains()` to search for the string "undefined", which is rarely what you want.
+ * @param position - The position within the string at which to begin searching for searchString. (Defaults to 0.)
+ * @returns `true` if the search string is found anywhere within the given string value, including
+ * when searchString is an empty string; otherwise, `false`.
+ * @throws TypeError If searchString is a regex.
+ */
+export const strContains: (value: string, searchString: string, position?: number) => boolean = strIncludes;
+
+
+/**
+ * The polyStrIncludes() method performs a case-sensitive search to determine whether one string
+ * may be found within another string, returning `true` or `false` as appropriate.
+ * @since 0.9.0
+ * @group String
+ * @group Polyfill
+ * @param value - The string value to be searched.
+ * @param searchString - A string to be searched for within the value. Cannot be a regex. All
+ * values that are not regexes are coerced to strings, so omitting it or passing undefined
+ * causes `strIncludes()` to search for the string "undefined", which is rarely what you want.
+ * @param position - The position within the string at which to begin searching for searchString. (Defaults to 0.)
+ * @returns `true` if the search string is found anywhere within the given string value, including
+ * when searchString is an empty string; otherwise, `false`.
+ * @throws TypeError If searchString is a regex.
+ */
+export function polyStrIncludes(value: string, searchString: string, position?: number): boolean {
+    if (isRegExp(searchString)) {
+        throwTypeError("'searchString' must not be a regular expression" + dumpObj(searchString))
+    }
+
+    return strIndexOf(value, asString(searchString), position) !== -1;
+}
---
+++ lib/test/src/common/string/includes.test.ts
@@ -0,0 +1,167 @@
+/*
+ * @nevware21/ts-utils
+ * https://github.com/nevware21/ts-utils
+ *
+ * Copyright (c) 2022 Nevware21
+ * Licensed under the MIT license.
+ */
+
+import { assert } from "chai";
+import { strIncludes, polyStrIncludes, strContains } from "../../../../src/string/includes";
+
+function _expectThrow(cb: () => void): Error {
+    try {
+        cb();
+    } catch (e) {
+        assert.ok(true, "Expected an exception to be thrown");
+        return e;
+    }
+}
+
+describe("strIncludes", () => {
+    it("null/undefined", () => {
+        _expectThrow(() => {
+            assert.equal(strIncludes(null as any, null as any), true);
+        });
+
+        _expectThrow(() => {
+            assert.equal(strIncludes(null as any, undefined as any), false);
+        });
+
+        _expectThrow(() => {
+            assert.equal(strIncludes(undefined as any, null as any), false);
+        });
+
+        _expectThrow(() => {
+            assert.equal(strIncludes(undefined as any, undefined as any), true);
+        });
+    });
+
+    it("Basic", () => {
+        assert.equal(strIncludes("Blue Whale", "blue"), false);
+
+        let value = "Hello Darkness my old friend, I've come to talk with you again";
+
+        assert.equal(strIncludes(value, "Hello Darkness"), true);
+        assert.equal(strIncludes(value, "Hello Darkness", 1), false);
+        assert.equal(strIncludes(value, "friend"), true);
+        assert.equal(strIncludes(value, "talk"), true);
+        assert.equal(strIncludes(value, "Old Friend", 1), false);
+        assert.equal(strIncludes(value, "old friend", 1), true);
+        assert.equal(strIncludes(value, "with you again"), true);
+        assert.equal(strIncludes(value, ""), true);
+    });
+
+    it("Search is not a string", () => {
+        let value = "Hello Darkness my old friend";
+        assert.equal(strIncludes(value, null as any), false);
+        assert.equal(strIncludes(value, undefined as any), false);
+        assert.equal(strIncludes("value is null", null as any), true);
+        assert.equal(strIncludes("undefined as the value", undefined as any), true);
+    });
+
+    it("regexp search", () => {
+        _expectThrow(() => {
+            strIncludes("Hello Darkness", (/Darkness/g) as any);
+        })
+    });
+});
+
+describe("strContains", () => {
+    it("null/undefined", () => {
+        _expectThrow(() => {
+            assert.equal(strContains(null as any, null as any), true);
+        });
+
+        _expectThrow(() => {
+            assert.equal(strContains(null as any, undefined as any), false);
+        });
+
+        _expectThrow(() => {
+            assert.equal(strContains(undefined as any, null as any), false);
+        });
+
+        _expectThrow(() => {
+            assert.equal(strContains(undefined as any, undefined as any), true);
+        });
+    });
+
+    it("Basic", () => {
+        assert.equal(strContains("Blue Whale", "blue"), false);
+
+        let value = "Hello Darkness my old friend, I've come to talk with you again";
+
+        assert.equal(strContains(value, "Hello Darkness"), true);
+        assert.equal(strContains(value, "Hello Darkness", 1), false);
+        assert.equal(strContains(value, "friend"), true);
+        assert.equal(strContains(value, "talk"), true);
+        assert.equal(strContains(value, "Old Friend", 1), false);
+        assert.equal(strContains(value, "old friend", 1), true);
+        assert.equal(strContains(value, "with you again"), true);
+        assert.equal(strContains(value, ""), true);
+    });
+
+    it("Search is not a string", () => {
+        let value = "Hello Darkness my old friend";
+        assert.equal(strContains(value, null as any), false);
+        assert.equal(strContains(value, undefined as any), false);
+        assert.equal(strContains("value is null", null as any), true);
+        assert.equal(strContains("undefined as the value", undefined as any), true);
+    });
+
+    it("regexp search", () => {
+        _expectThrow(() => {
+            strContains("Hello Darkness", (/Darkness/g) as any);
+        })
+    });
+});
+
+describe("polyStrIncludes", () => {
+    it("null/undefined", () => {
+        _expectThrow(() => {
+            assert.equal(polyStrIncludes(null as any, null as any), true);
+        });
+
+        _expectThrow(() => {
+            assert.equal(polyStrIncludes(null as any, undefined as any), false);
+        });
+
+        _expectThrow(() => {
+            assert.equal(polyStrIncludes(undefined as any, null as any), false);
+        });
+
+        _expectThrow(() => {
+            assert.equal(polyStrIncludes(undefined as any, undefined as any), true);
+        });
+    });
+
+    it("Basic", () => {
+        assert.equal(polyStrIncludes("Blue Whale", "blue"), false);
+
+        let value = "Hello Darkness my old friend, I've come to talk with you again";
+
+        assert.equal(polyStrIncludes(value, "Hello Darkness"), true);
+        assert.equal(polyStrIncludes(value, "Hello Darkness", 1), false);
+        assert.equal(polyStrIncludes(value, "friend"), true);
+        assert.equal(polyStrIncludes(value, "talk"), true);
+        assert.equal(polyStrIncludes(value, "Old Friend", 1), false);
+        assert.equal(polyStrIncludes(value, "old friend", 1), true);
+        assert.equal(polyStrIncludes(value, "with you again"), true);
+        assert.equal(polyStrIncludes(value, ""), true);
+    });
+
+    it("Search is not a string", () => {
+        let value = "Hello Darkness my old friend";
+        assert.equal(polyStrIncludes(value, null as any), false);
+        assert.equal(polyStrIncludes(value, undefined as any), false);
+        assert.equal(strIncludes("value is null", null as any), true);
+        assert.equal(strIncludes("undefined as the value", undefined as any), true);
+    });
+
+    it("regexp search", () => {
+        _expectThrow(() => {
+            polyStrIncludes("Hello Darkness", (/Darkness/g) as any);
+        })
+    });
+});
+
