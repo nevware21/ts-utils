b5fc11b8478357e72dad884807ef514ba63a06c0
nev21 <82737406+nev21@users.noreply.github.com>
2023-03-26 17:47:02 -0700

    Add writable Lazy support and lazy option to objDefine (#166)

--- lib/src/helpers/lazy.ts
+++ lib/src/helpers/lazy.ts
@@ -39,13 +39,14 @@ export interface ILazyValue<T> {
 }
 
 /**
- * Create and return an {@link ILazyValue} instance which will cache and return the value returned
+ * Create and return an readonly {@link ILazyValue} instance which will cache and return the value returned
  * by the callback function. The callback function will only be called once, multiple access of the value
  * does not cause re-execution of the callback as the result from the first call is cached internally.
  * @since 0.4.5
  * @group Lazy
  * @param cb - The callback function to fetch the value to be lazily evaluated and cached
- * @returns A new {@link ILazyValue} instance which wraps the callback and will be used to cache the result of the callback
+ * @returns A new readonly {@link ILazyValue} instance which wraps the callback and will be used to cache
+ * the result of the callback
  * @example
  * ```ts
  * // This does not cause the evaluation to occur
@@ -106,4 +107,84 @@ export function getLazy<T>(cb: () => T): ILazyValue<T> {
 export function setBypassLazyCache(newValue: boolean) {
     _fetchLazyTestHooks && _fetchLazyTestHooks();
     _globalLazyTestHooks.lzy = newValue;
+}
+
+/**
+ * Create and return a writable {@link ILazyValue} instance which will cache and return the value returned
+ * by the callback function. The callback function will only be called once, multiple access of the value
+ * does not cause re-execution of the callback as the result from the first call is cached internally. The
+ * value may be set as many times as required, if the callback has not been called when you set the value
+ * it will never get called.
+ * @since 0.9.4
+ * @group Lazy
+ * @param cb - The callback function to fetch the value to be lazily evaluated and cached
+ * @returns A new writable {@link ILazyValue} instance which wraps the callback and will be used to cache
+ * the result of the callback
+ * @example
+ * ```ts
+ * // This does not cause the evaluation to occur
+ * let cachedValue = getWritableLazy(() => callSomeExpensiveFunction());
+ * let theValue;
+ *
+ * // Just checking if there is an object still does not cause the evaluation
+ * if (cachedValue) {
+ *     // This will cause the evaluation to occur and the result will be cached
+ *     theValue = cachedValue.v;
+ * }
+ *
+ * // Accessing the value again will not cause the re-evaluation to occur, it will just return the same
+ * // result value again.
+ * theValue === cachedValue.v;  // true
+ *
+ * // Setting the value
+ * let cachedValue = getWritableLazy(() => callSomeExpensiveFunction());
+ * let theValue = "new Value";
+ *
+ * // Just checking if there is an object still does not cause the evaluation
+ * if (cachedValue) {
+ *     // This will set the value to be set explicitly and the callback
+ *     // will now never occur and the result will be cached
+ *     cachedValue.v = theValue;
+ * }
+ *
+ * // Accessing the value again will cause the previously set value to be returned.
+ * theValue === cachedValue.v;  // true
+ * ```
+ */
+export function getWritableLazy<T>(cb: () => T): ILazyValue<T> {
+    let lazyValue = { } as ILazyValue<T>;
+    _fetchLazyTestHooks && _fetchLazyTestHooks();
+    lazyValue.b = _globalLazyTestHooks.lzy;
+
+    let _setValue = (newValue: T) => {
+        // Just replace the value
+        objDefineProp(lazyValue, "v", {
+            value: newValue,
+            writable: true
+        });
+
+        if (lazyValue.b) {
+            delete lazyValue.b;
+        }
+    };
+
+    objDefineProp(lazyValue, "v", {
+        configurable: true,
+        get: function () {
+            let result = cb();
+            if (!_globalLazyTestHooks.lzy) {
+                // Just replace the value
+                _setValue(result);
+            }
+            
+            if (_globalLazyTestHooks.lzy && lazyValue.b !== _globalLazyTestHooks.lzy) {
+                lazyValue.b = _globalLazyTestHooks.lzy;
+            }
+
+            return result;
+        },
+        set: _setValue
+    });
+
+    return lazyValue;
 }
--- lib/test/src/common/helpers/lazy.test.ts
+++ lib/test/src/common/helpers/lazy.test.ts
@@ -7,10 +7,10 @@
  */
 
 import { assert } from "chai";
-import { getLazy, setBypassLazyCache } from "../../../../src/helpers/lazy";
+import { getLazy, getWritableLazy, setBypassLazyCache } from "../../../../src/helpers/lazy";
 
 
-describe("check lazy caching", () => {
+describe("check readonly lazy caching", () => {
 
     it("Check lazy", () => {
         let lazyCalled = 0;
@@ -77,6 +77,126 @@ describe("check lazy caching", () => {
         let lazyValue = "Hello";
 
         let value = getLazy(() => {
+            lazyCalled++;
+            return lazyValue;
+        });
+
+        setBypassLazyCache(true);
+
+        assert.equal(value.v, "Hello", "checking the initial value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+
+        setBypassLazyCache(false);
+
+        // The first access after restoring caching will still bypass
+        lazyValue = "Darkness";
+        assert.equal(value.v, "Darkness", "checking the initial value");
+        assert.equal(2, lazyCalled, "Check that the lazy function was only called once");
+
+        // But subsequent requests would still return the last cached value
+        lazyValue = "My";
+        assert.equal(value.v, "Darkness", "checking the initial value");
+        assert.equal(2, lazyCalled, "Check that the lazy function was only called once");
+    });
+});
+
+describe("check writable lazy caching", () => {
+
+    it("Check lazy", () => {
+        let lazyCalled = 0;
+        let lazyValue = "Hello";
+
+        let value = getWritableLazy(() => {
+            lazyCalled++;
+            return lazyValue;
+        });
+
+        setBypassLazyCache(false);
+
+        assert.equal(value.v, "Hello", "checking the initial value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+
+        lazyValue = "Darkness";
+
+        assert.equal(value.v, "Hello", "checking the initial value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+
+        value.v = lazyValue;
+        assert.equal(value.v, "Darkness", "checking the updated value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+    });
+
+    it("Check setting before lazy", () => {
+        let lazyCalled = 0;
+        let lazyValue = "Hello";
+
+        let value = getWritableLazy(() => {
+            lazyCalled++;
+            return lazyValue;
+        });
+
+        setBypassLazyCache(false);
+
+        value.v = "my old friend";
+
+        assert.equal(value.v, "my old friend", "checking the updated value");
+        assert.equal(0, lazyCalled, "Check that the lazy function was only called once");
+
+        lazyValue = "Darkness";
+
+        assert.equal(value.v, "my old friend", "checking the initial value");
+        assert.equal(0, lazyCalled, "Check that the lazy function was only called once");
+
+        value.v = lazyValue;
+        assert.equal(value.v, "Darkness", "checking the updated value");
+        assert.equal(0, lazyCalled, "Check that the lazy function was only called once");
+    });
+
+    it("Check bypass lazy before creation", () => {
+        let lazyCalled = 0;
+        let lazyValue = "Hello";
+
+        let value = getWritableLazy(() => {
+            lazyCalled++;
+            return lazyValue;
+        });
+
+        setBypassLazyCache(true);
+
+        assert.equal(value.v, "Hello", "checking the initial value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+
+        lazyValue = "Darkness";
+        assert.equal(value.v, "Darkness", "checking the initial value");
+        assert.equal(2, lazyCalled, "Check that the lazy function was only called once");
+    });
+
+    it("Check bypass lazy after creation", () => {
+        let lazyCalled = 0;
+        let lazyValue = "Hello";
+
+        let value = getWritableLazy(() => {
+            lazyCalled++;
+            return lazyValue;
+        });
+
+        setBypassLazyCache(false);
+
+        assert.equal(value.v, "Hello", "checking the initial value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+
+        setBypassLazyCache(true);
+        lazyValue = "Darkness";
+
+        assert.equal(value.v, "Hello", "checking the initial value");
+        assert.equal(1, lazyCalled, "Check that the lazy function was only called once");
+    });
+
+    it("Check disabling bypass lazy after creating with bypass enabled", () => {
+        let lazyCalled = 0;
+        let lazyValue = "Hello";
+
+        let value = getWritableLazy(() => {
             lazyCalled++;
             return lazyValue;
         });
